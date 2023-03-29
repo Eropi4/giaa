d8acdff0f371117beb01897002f6e2c5c3f2c2ef
Yehor Melnykov <y.melnykov@p1k.co.uk>
2023-03-29 11:28:46 +0200

    Calc remaining URLs by Pacific Time

--- modules/utils.js
+++ modules/utils.js
@@ -2,8 +2,9 @@ let mongoose = require('mongoose');
 let Url = require("../models/url");
 
 module.exports.remainingUrls = function remainingUrls(callback) {
-  var dateFrom = new Date();
-  dateFrom.setUTCHours(0,0,1,0);
+  //API quota resets by Pacific Time, offset UTC date by 8 hours, add 1 second drift
+  var dateFrom = new Date(Date.now() - 1000 * 3600 * 8 - 1000);
+  dateFrom.setUTCHours(8,0,1,0);
   Url.countDocuments({'notifytime' : { $gt : dateFrom}, 'status': 'updated'}, function( err, count){
     if (err) {
       callback(err, null);
